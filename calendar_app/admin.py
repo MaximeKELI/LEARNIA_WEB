4b58f21ec26caf8f0ca236ffd592be16e5c71337
MaximeKELI <presidentnetero01@gmail.com>
2025-12-28 23:37:25 +0000

    LEARNIA

---
+++ calendar_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import EvenementScolaire
+
+
+@admin.register(EvenementScolaire)
+class EvenementScolaireAdmin(admin.ModelAdmin):
+    list_display = ['titre', 'type_evenement', 'date_debut', 'user', 'public', 'created_at']
+    list_filter = ['type_evenement', 'public', 'date_debut']
+    search_fields = ['titre', 'description', 'user__username']
+    date_hierarchy = 'date_debut'
+
---
+++ calendar_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'calendar'
+
+urlpatterns = [
+    path('', views.calendar_view, name='view'),
+    path('create/', views.event_create, name='create'),
+    path('<int:event_id>/edit/', views.event_edit, name='edit'),
+    path('<int:event_id>/delete/', views.event_delete, name='delete'),
+]
+
