0d7ac578d1233dc04150e9c0d7863d17b38819b3
MaximeKELI <presidentnetero01@gmail.com>
2025-12-28 22:05:34 +0000

    LEARNIA

---
+++ qcm/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'qcm'
+
+urlpatterns = [
+    path('', views.qcm_index, name='index'),
+    path('generate/', views.generate_qcm, name='generate'),
+    path('<int:qcm_id>/', views.qcm_detail, name='detail'),
+    path('<int:qcm_id>/submit/', views.submit_qcm, name='submit'),
+]
+
