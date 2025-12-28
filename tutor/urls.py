bd072a1263301548ace21e439ca52d4c44a37eec
MaximeKELI <presidentnetero01@gmail.com>
2025-12-28 22:04:07 +0000

    LEARNIA

---
+++ tutor/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'tutor'
+
+urlpatterns = [
+    path('', views.tutor_index, name='index'),
+    path('conversation/<int:conversation_id>/', views.conversation_detail, name='conversation_detail'),
+    path('send-message/', views.send_message, name='send_message'),
+    path('new/', views.new_conversation, name='new_conversation'),
+]
+
