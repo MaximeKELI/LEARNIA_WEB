5f712d91db9ef6e653776acc168b235e9d2acb61
MaximeKELI <presidentnetero01@gmail.com>
2025-12-28 22:10:50 +0000

    LEARNIA

---
+++ ocr/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'ocr'
+
+urlpatterns = [
+    path('', views.ocr_index, name='index'),
+    path('upload/', views.upload_devoir, name='upload'),
+    path('<int:devoir_id>/', views.devoir_detail, name='devoir_detail'),
+]
+
