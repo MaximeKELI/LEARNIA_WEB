c02b8a12660964481b9fbfe4a5506846d08ade37
MaximeKELI <presidentnetero01@gmail.com>
2025-12-28 22:04:14 +0000

    LEARNIA

---
+++ tutor/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Conversation, Message
+
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ['titre', 'user', 'chapitre', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['titre', 'user__username']
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['conversation', 'role', 'created_at']
+    list_filter = ['role', 'created_at']
+    search_fields = ['contenu']
+
